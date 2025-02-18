2fbc23eb95a4404e19c76bfd5f048595c4e86d7e
mukut-d <mukut.m.das2000@gmail.com>
2025-02-18 21:18:51 +0530

    added text animations

---
+++ index.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".multiple-text", {
+  strings: [
+    "Associate Software Engineer.",
+    "Full Stack Developer.",
+    "Technocrat.",
+    "Undergraduate Student.",
+    "Fresher.",
+    "2024 CS Graduate.",
+    "Lifelong Learner.",
+    "Brother.",
+    "Son.",
+    "Human Being.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 500,
+  loop: true,
+});
